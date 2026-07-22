548186808b34124c6b549f550610fd1453097a1a
Amartya Gupta <amartagupta07@gmail.com>
2026-07-22 12:30:50 +0530

    Time: 0 ms (100.00%) | Memory: 69 MB (79.31%) - LeetSync

---
+++ 2591-frog-jump-ii/frog-jump-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxJump(vector<int>& stones) {
+        int n = stones.size();
+        int result = stones[1] - stones[0];
+        for (int i = 2; i < n; i++)
+            result = max(result, stones[i] - stones[i-2]);
+
+        return result;
+    }
+};
